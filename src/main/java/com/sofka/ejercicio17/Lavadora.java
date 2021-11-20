cbe2d2e07e01fb31635f89a74190049ba3ad255b
EmmilioV <emmiliov.p@gmail.com>
2021-11-20 02:07:23 -0500

    SubClass Lavadora for excercise 17 created

--- src/main/java/com/sofka/ejercicio17/Lavadora.java
+++ src/main/java/com/sofka/ejercicio17/Lavadora.java
@@ -1,4 +1,28 @@
 package com.sofka.ejercicio17;
 
-public class Lavadora {
+public class Lavadora extends Electrodomestico{
+    private double carga;
+
+    public Lavadora() {
+        this.carga = 5;
+    }
+
+    public Lavadora(double precioBase, double peso) {
+        super(precioBase, peso);
+        this.carga = 5;
+    }
+
+    public Lavadora(double precioBase, String color, char consumoEnergetico, double peso, double carga) {
+        super(precioBase, color, consumoEnergetico, peso);
+        this.carga = carga;
+    }
+
+    public double getCarga() {
+        return carga;
+    }
+
+    @Override
+    public void PrecioFinal() {
+        super.PrecioFinal();
+    }
 }
